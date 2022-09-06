aca1b54280d5e76d3205f0ab9b7f82d8bf976806
Agustin Chiappara <agus.chiappara@gmail.com>
2022-09-05 23:06:28 -0300

    carro updateado responsive

--- app.js
+++ app.js
@@ -82,33 +82,33 @@ function addCartClicked(event){
   updatetotal();
 }
 function addProductToCart(title, price, productImg) {
-  var cartShopBox = document.createElement('div');
+  var cartShopBox = document.createElement("div");
   cartShopBox.classList.add("cart-box");
   var cartItems = document.getElementsByClassName("cart-content")[0];
-  var cartItemsNames = cartItems.getElementsByClassName('cart-product-title');
+  var cartItemsNames = cartItems.getElementsByClassName("cart-product-title");
   for (var i = 0; i < cartItemsNames.length; i++) {
-    if (cartItemsNames[i].innerText == title){
-    alert("Ya has añadido este item en el carrito"); 
+    if (cartItemsNames[i].innerText == title) {
+      alert("Ya has añadido este item en el carrito"); 
     return;
   }
 }
 var cartBoxContent = `
-<img src="${productImg}" class="cart-img">
+<img src="${productImg}" alt="" class="cart-img">
 <div class="detail-box">
   <div class="cart-product-title">${title}</div>
   <div class="cart-price">${price}</div>
   <input type="number" value="1" class="cart-quantity">
 </div>
 <!---- remove cart --->
-<i class="bx bxs-trash-alt cart-remove"></i>`
+<i class='bx bxs-trash-alt cart-remove'></i>`;
 cartShopBox.innerHTML = cartBoxContent;
 cartItems.append(cartShopBox);
 cartShopBox
-.getElementsByClassName('cart-remove')[0]
-.addEventListener('click', removeCartItem);
+  .getElementsByClassName("cart-remove")[0]
+  .addEventListener("click", removeCartItem);
 cartShopBox
-.getElementsByClassName('cart-quantity')[0]
-.addEventListener('change', quantityChanged);
+  .getElementsByClassName("cart-quantity")[0]
+  .addEventListener("change", quantityChanged);
 }
   // Actualizacion total
   function updatetotal(){
